2adcae8e2fb2f730c22db00212a46422fcf9a194
Moshe Berdugo <bomberdugo@gmail.com>
2017-04-11 13:05:25 +0300

    Node js server set up

---
+++ server.js
@@ -0,0 +1,27 @@
+var express= require('express');
+var path = require('path');
+var bodyparser = require('body-parser');
+
+var index = require('./routes/index');
+var tasks = require('./routes/tasks');
+
+var port = 3000;
+var app = express();
+
+//Views engine
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+app.engine('html',require('ejs').renderFile);
+
+//Set static folder
+app.use(express.static(path.join(__dirname,'client')));
+
+
+//bodyparser middleware
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:false}));
+
+app.use('/',index);
+app.use('/api',tasks);
+
+app.listen(port,function(){console.log("server listening on port: " + port)});
